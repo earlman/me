307d3541c1adc53bd8501e6b70a7cdae6c8a905e
earlman <earl.mangulabnan@gmail.com>
2019-12-27 16:39:41 -0600

    feat(site): add navbar

---
+++ site/src/components/Navbar.vue
@@ -0,0 +1,121 @@
+<template>
+	<header>
+		<nav>
+			<ul class="menu">
+				<div class="logo">
+					<g-link to="/">
+						<img
+							src="../favicon.png"
+							alt="Metal Law Group Logo"
+							height="100"
+							width="100"
+						/>
+					</g-link>
+				</div>
+				<li
+					v-for="item in menu"
+					:key="item"
+				>
+					<g-link :to="item.url">{{item.name}}</g-link>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "navbar",
+	data() {
+		return {
+			menu: [
+				{ name: "action", url: "/action" },
+				{ name: "experience", url: "/experience" }
+			]
+		};
+	}
+};
+</script>
+
+<style lang="sass" scoped>
+@import '../styles/04 - Layout/media' 
+
+header
+    justify-self: center
+    width: 100vw
+    display: flex
+    flex-direction: column
+    align-items: center
+
+    .menu
+        width: 100%
+        display: flex
+        justify-content: space-around            
+        align-items: flex-end
+        flex-wrap: wrap
+        width: 88vw
+        max-width: 1400px
+        margin-top: 20px
+        @include landscape
+            justify-content: space-between
+
+
+        .logo
+            max-width: 300px
+            margin-bottom: var(--space-sm)
+            margin-top: var(--space-md)
+            
+        ul
+            display: flex
+            flex-wrap: wrap
+            justify-content: center
+            padding: 0
+            margin-bottom: 0
+            line-height: 1
+
+            li:hover > ul,
+            li:focus-within > ul,
+            li ul:hover
+                visibility: visible
+                opacity: 1    
+                display: block           
+
+            li
+                display: block
+                transition-duration: .5s
+                background: var(--color-b)
+                position: relative
+                float: left
+
+                a
+                    padding: var(--space-xs)
+                    display: block
+
+                &:hover,
+                &:focus-within
+                    // cursor: pointer
+                    background: var(--color-a)
+
+                &:focus-within a
+                    outline: none
+
+                .empty 
+                    cursor: default
+                ul
+                    position: absolute
+                    z-index: 10
+                    transition: all 0.5s ease
+                    visibility: hidden
+                    opacity: 0
+                    display: none
+                    left: 0
+                    li //submenu
+                        min-width: 25ch
+                            
+
+
+li
+    list-style: none
+
+
+</style>

21de7b0b00808e489738837919370e9b6cd048cd
earlman <earl.mangulabnan@gmail.com>
2019-12-30 13:46:37 -0600

    fix(site): load sass layout mixins for each file

--- site/gridsome.config.js
+++ site/gridsome.config.js
@@ -3,8 +3,27 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require('path')
 
+function addStyleResource(rule) {
+    rule.use('style-resource')
+        .loader('style-resources-loader')
+        .options({
+            patterns: [
+                path.resolve(__dirname, './src/styles/04 - Layout/_layout.sass'),
+            ],
+        })
+}
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+    chainWebpack(config) {
+        // Load variables for all vue-files
+        const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+        types.forEach(type => {
+            addStyleResource(config.module.rule('sass').oneOf(type))
+        })
+
+    },
+    siteName: 'Gridsome',
+    plugins: []
 }

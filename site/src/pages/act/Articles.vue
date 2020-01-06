bc59e12cc0311f4c73477a60113bf784b1fce89e
earlman <earl.mangulabnan@gmail.com>
2020-01-06 16:43:05 -0600

    feat(site): add articles query

--- site/src/pages/act/Articles.vue
+++ site/src/pages/act/Articles.vue
@@ -1,8 +1,28 @@
 <template>
 	<Layout>
-		<h1>Articles</h1>
+		<h1>Article Template</h1>
+		<p
+			v-for="article in $page.articles.edges"
+			:key="article.node.id"
+		>
+			<g-link :href="article.node.path">{{article.node.title}}</g-link>
+		</p>
 	</Layout>
 </template>
+
+<page-query>
+query {
+    articles: allArticle {
+        edges {
+            node {
+                id
+                title
+                path
+            }
+        }
+    }
+}
+</page-query>
 
 <script>
 export default {};

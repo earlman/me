e3e71bb0ea4e6f1a95bf8c2ed4e29b446ef6ffa7
earlman <earl.mangulabnan@gmail.com>
2020-01-07 21:36:47 -0600

    feat(site): create MetaInfo component

---
+++ site/src/components/MetaInfo.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<p>{{date}}</p>
+		<p>{{time}}</p>
+		<ul>
+			<li
+				v-for="(tag, index) in tags"
+				:key="index"
+			>{{tag}} |</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+const moment = require("moment");
+
+export default {
+	props: {
+		datetime: {
+			type: String,
+			default: "YYYYDDMMTHH:mm"
+		},
+		tags: {
+			type: Array,
+			default: ["Tag 1", "Tag 2", "Tag 3"]
+		},
+		showTime: {
+			type: Boolean,
+			default: false
+		}
+	},
+
+	computed: {
+		date() {
+			return moment(this.datetime).format("MMMM D, YYYY");
+		},
+		time() {
+			return moment(this.datetime).format("h:mm a");
+		}
+	}
+};
+</script>
+
+<style lang="css" scoped>
+li {
+	list-style-type: none;
+	display: inline-block;
+}
+</style>

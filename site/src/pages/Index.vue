7676424c64056b459b67b5ea1eb3239e0bd835b2
earlman <earl.mangulabnan@gmail.com>
2020-01-13 15:07:21 -0600

    refactor(site): Index.vue no longer uses Default Layout

--- site/src/pages/Index.vue
+++ site/src/pages/Index.vue
@@ -1,27 +1,31 @@
 <template>
 	<div class="page-container">
-		<section class="hero">
-			<div class="profilepic">
-				<g-image
-					src="~/profilepicture.jpg"
-					alt="Profile Picture"
-					height="600"
-					width="400"
-				/>
-			</div>
-			<div v-html="$page.pageContent.content"></div>
-		</section>
-		<Layout
-			class="layout"
-			coloredNav
-		></Layout>
+		<div class="hero-container">
+			<section class="hero">
+				<div class="profilepic">
+					<g-image
+						src="~/profilepicture.jpg"
+						alt="Profile Picture"
+						height="600"
+						width="400"
+					/>
+				</div>
+				<div v-html="$page.pageContent.content"></div>
+			</section>
+		</div>
+		<Navbar colored />
 	</div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
 	metaInfo: {
 		title: "Hello, world!"
+	},
+	components: {
+		Navbar
 	}
 };
 </script>
@@ -36,13 +40,21 @@ query {
 
 <style lang="sass" scoped>
 
+
 .hero
+    &-container
+        display: flex
+        flex-direction: column
+        align-items: center
+
     width: var(--layout-width)
     max-width: var(--layout-max-width)
     display: flex
     align-items: center
     justify-content: center    
     min-height: 50vh
+
+
 
     .profilepic
         @include portrait
@@ -52,13 +64,9 @@ query {
             margin-top: var(--space-lg)
             margin-bottom: var(--space-lg)
 
-   
-
 .page-container
     min-height: 100vh
-    display: flex
-    flex-direction: column
-    align-items: center
+
 
 .profilepic img
     border-radius: 2px

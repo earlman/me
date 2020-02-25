7b26997b156fa931b53832c5c4deaa00cf1eb617
earlman <earl.mangulabnan@gmail.com>
2020-02-25 12:51:14 -0600

    feat(site): improve styles on articles

--- site/src/templates/Article.vue
+++ site/src/templates/Article.vue
@@ -1,7 +1,13 @@
 <template>
 	<Layout>
 		<article>
-			<h1>{{$page.article.title}}</h1>
+			<div class="title-block">
+				<h1 class="title">{{$page.article.title}}</h1>
+				<h4
+					class="subtitle"
+					v-if="$page.article.subtitle"
+				>{{$page.article.subtitle}}</h4>
+			</div>
 			<div
 				v-html="$page.article.content"
 				class="article-content"
@@ -18,6 +24,7 @@ export default {};
 query ($id: ID!) {
     article(id: $id) {
         title
+        subtitle
         content
         excerpt
         path
@@ -27,11 +34,26 @@ query ($id: ID!) {
 
 <style lang="sass" scoped>
 
-
     .article-content
 
-        ::v-deep *
-            font-size: 20px
+        ::v-deep
+
+            h2
+                font-size: var(--d-xl)
+                margin-bottom: var(--space-xs)
+                margin-top: var(--space-md)
+
+
+    .title-block
+        margin-bottom: var(--space-md)
+
+        .title
+            margin-bottom: var(--space-xs)
+        .subtitle
+            font-family: var(--font-primary)
+            font-weight: 400
+            
+            
 
 
 </style>

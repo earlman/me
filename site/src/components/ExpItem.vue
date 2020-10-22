0b6f69cc5a285b13b46bcb5c62c5011b34f98148
earlman <earl.mangulabnan@gmail.com>
2020-10-22 15:43:00 -0500

    style experience tables

--- site/src/components/ExpItem.vue
+++ site/src/components/ExpItem.vue
@@ -1,9 +1,15 @@
 <template>
-	<div>
-		{{formatTime(item.node.date)}}
-		<g-link :to="item.node.path">{{item.node.title}}</g-link>
-		{{item.node.rating.score}}
-	</div>
+	<tr>
+		<td class="date">
+			{{ formatTime(item.node.date) }}
+		</td>
+		<td class="title">
+			<g-link :to="item.node.path">{{ item.node.title }}</g-link>
+		</td>
+		<td class="rating">
+			{{ item.node.rating.score }}
+		</td>
+	</tr>
 </template>
 
 <script>
@@ -13,8 +19,8 @@ const moment = require("moment");
 export default {
 	props: {
 		item: {
-			type: Object
-		}
+			type: Object,
+		},
 	},
 	methods: {
 		formatTime(dateTime) {
@@ -27,17 +33,47 @@ export default {
 			const threshold2 = moment().subtract(1, "year"); // 1 year ago
 			// if time is before 2 weeks ago, display date, else display time ago
 			if (time.isBefore(threshold)) {
-				return time.format("MMM D");
+				return time.format("MMM. D");
 			} else if (time.isBefore(threshold2)) {
 				return time.format("MMM D, YYYY");
 			} else {
 				return time.fromNow();
 			}
-		}
-	}
+		},
+	},
 };
 </script>
 
 <style lang="sass" scoped>
+.date, .title, .rating
+    border-top: 1px solid rgba(72, 80, 116, .1)
+    border-bottom: 1px solid rgba(72, 80, 116, .1)
+    border-left: none
+    border-right: none
+
+.date
+    font-family: var(--font-secondary)
+    color: var(--color-a)
+    font-size: var(--d-sm)
+    width: 10ch
+    padding-left: 0
+
+.title
+    font-familiy: var(--font-primary)
+
+    a
+        color: var(--color-t)
+        font-size: var(--d-sm)
+        font-weight: 400
+
+    a:hover
+        color: var(--color-a)
+
+
+.rating
+    font-family: var(--font-secondary)
+    color: var(--color-t-c)
+    font-size: var(--d-sm)
+
 
 </style>
